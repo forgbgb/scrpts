e652e991116f7ef292157ccd02658749515bfef0
Sneha Achar <Sneha.Achar@tpvision.com>
2018-08-03 14:46:40 +0530

    changes

--- 62_modifiedSMTVMain.sh
+++ 62_modifiedSMTVMain.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x 
+#!/bin/bash 
 #cd /htv_archive/2K18HTV/MSAF_2k18_EU_HTV_M
 #repo --trace sync -j8 
 #echo "================== MIRRORDONE "
@@ -23,30 +23,33 @@ LASTEXECUTION=1532422512
 LASTEXECUTION=1532422512
 CURRENTEXECUTION=`date +%s` 
 SYNCSTATUS=true ; BLDSTATUS=true ; CONFLICTS=false; COUNT=false
-PD=/mnt/localdata/build.pc/Archive/Scrum2SMTV
-#PD=/mnt/localdata/localhome/build.pc/Archive/SMTVMain
+PD=/mnt/localdata/localhome/build.pc/Archive/SMTVMain
 echo " " > ${PD}/a ; cd $PD
 
 case "$1" in
-      Channelapp) echo "channelapp"
+      Channelapp) 
 	BRANCH="2k18_mtk_msaf_kane_n_devprod_EU_HTV_Scrum_Channelapp"
 	MANIFEST="default_Scrum_channelapp.xml"	
+	echo "$1 $BRANCH $MANIFEST"
         ;;
-      SMTV) echo "Smtv"
+      SMTV) 
 	BRANCH="2k18_mtk_msaf_kane_n_devprod_EU_HTV_Scrum_SmartTV"
 	MANIFEST="default_Scrum_SmartTV.xml"	
+        echo "$1 $BRANCH $MANIFEST"
         ;;
-      RFBasedCloning) echo "RFBasedCloning"
+      RFBasedCloning) 
 	BRANCH="2k18_mtk_msaf_kane_n_devprod_EU_HTV_Scrum_RFBasedCloning"
 	MANIFEST="default_Scrum_RFBasedCloning.xml"	
+	echo "$1 $BRANCH $MANIFEST"
         ;;
       *) echo "Default smtv "
 	BRANCH="2k18_mtk_msaf_kane_n_devprod_EU_HTV_Scrum_SmartTV"
 	MANIFEST="default_Scrum_SmartTV.xml"	
+	echo "$1 $BRANCH $MANIFEST"
         ;;
 esac
 
-#${REPO} init  -u ssh://url/tpv/platform/manifest -b 2k18_mtk_msaf_kane_n_devprod_EU_HTV  --reference=/htv_archive/2K18HTV/MSAF_2k18_EU_HTV_M
+${REPO} init  -u ssh://url/tpv/platform/manifest -b 2k18_mtk_msaf_kane_n_devprod_EU_HTV  --reference=/htv_archive/2K18HTV/MSAF_2k18_EU_HTV_M
 ${REPO} forall -c ' git clean -xfd; git reset --hard  ; git rebase --abort '
 ${REPO}   --trace sync -d -f -c 
    if [  $? !=  0 ]  ; then
@@ -55,7 +58,7 @@ ${REPO}   --trace sync -d -f -c
      echo ".................1" 
     exit 0
    fi
-   echo "Sync done "
+echo "Sync done "
 filename=`mktemp`
 cat ${PD}/.repo/manifests/${MANIFEST} | grep revision | grep -v  "tags" | tr -s " "  "~" | cut -d "~" -f 4 | cut -d "\"" -f 2 >  ${filename}
 for item in `cat ${filename} `
@@ -67,22 +70,27 @@ do
   if [  $? != 0 ]  ; then
      CONFLICTS=true
      echo " Conflicts found in ${item} during ${SMTVTIP} commit pick " >> ${PD}/a
+     echo " .................. $item " >> ${PD}/a
      echo "$MERGESTAT "  >> ${PD}/a  
      git merge --abort 
   else
+     echo " .................. $item " >> ${PD}/a
      echo "$MERGESTAT "  >> ${PD}/a
      if [ ` git log --oneline -10 --pretty="%cn %s" origin/2k18_mtk_msaf_kane_n_devprod_EU_HTV..HEAD  | egrep -vi "sync|build\.pc" | wc -l `  -gt 0  ]  ; then 
-     COUNT=true  ; fi 
+       COUNT=true  ; 
+     else
+        repo sync . 
+     fi 
    fi 
   cd - 
 done
  echo " ........... $CONFLICTS  $COUNT " 
   if   $CONFLICTS    ; then
-   /usr/bin/mail -s " $SCRUM Conflicts found .. stopping bld process   $HOSTNAME" ${MAILIDS}  < ${PD}/a
+   /usr/bin/mail -s " $SCRUM Mainline  Conflicts found .. stopping bld process   $HOSTNAME" ${MAILIDS}  < ${PD}/a
    $(which rm) ${filename}
     exit 0
     elif ! $COUNT  ; then
-      /usr/bin/mail -s " $SCRUM No commits found .. stopping bld process   $HOSTNAME" ${MAILIDS}  < ${PD}/a
+      /usr/bin/mail -s " $SCRUM Mainline No commits found .. stopping bld process   $HOSTNAME" ${MAILIDS}  < ${PD}/a
        $(which rm) ${filename}
       exit 0
     else
@@ -91,18 +99,18 @@ ${REPO}  forall -c '  if [ `git log origin/2k18_mtk_msaf_kane_n_devprod_EU_HTV..
 
    echo "================ SYNC DONE =============" 
 
-#   cd android/n-base/
-#   cd  prebuilts/sdk/tools
-#   ./jack-admin stop-server
-#   ./jack-admin kill-server
-#   ./jack-admin install-server jack-launcher.jar   jack-server-4.8.ALPHA.jar
-#   cd ${PD}/android/n-base/
-#   rm -Rf out/ __TP*
-#   source ./build/envsetup.sh
-#   echo "=================== LUNCH ==============" 
-#   lunch PH8M_HE_5596-userdebug
-#   export USER=$(whoami)
-#   make -j mtk_clean >  make.log 2>&1  ; make -j mtk_build > make.log 2>&1 ;  cd device/tpv/common/sde/upg ; sh generate_upg_lonelyrun_cmdmode.sh 2 ph8m_he_5596; sh generate_upg_lonelyrun_cmdmode.sh 3 ph8m_he_5596 
+   cd android/n-base/
+   cd  prebuilts/sdk/tools
+   ./jack-admin stop-server
+   ./jack-admin kill-server
+   ./jack-admin install-server jack-launcher.jar   jack-server-4.8.ALPHA.jar
+   cd ${PD}/android/n-base/
+   rm -Rf out/ __TP*
+   source ./build/envsetup.sh
+   echo "=================== LUNCH ==============" 
+   lunch PH8M_HE_5596-userdebug
+   export USER=$(whoami)
+   make -j mtk_clean >  make.log 2>&1  ; make -j mtk_build > make.log 2>&1 ;  cd device/tpv/common/sde/upg ; sh generate_upg_lonelyrun_cmdmode.sh 2 ph8m_he_5596; sh generate_upg_lonelyrun_cmdmode.sh 3 ph8m_he_5596 
 
 echo "Hai All,"  > ${PD}/tuxpath
 echo "Logs for build is located @ $TUXPATH"  >> ${PD}/tuxpath
--- ImgVidFileRename.sh
+++ ImgVidFileRename.sh
@@ -1,72 +1,43 @@
-
 clear
 
 ############# For VID/IMG-<dt>-WA*.ext type of files  and  IMG_<dt>_<time>.ext 
-for l in `/bin/cat a.txt`
+Folder=Photos_Videos/Photos_Jan13
+newfolder=SortedPhotos_Videos
+$(which find) $Folder  -name *.jpg  > $Folder/a.txt 
+
+for l in `$(which cat) ${Folder}/a.txt`
 do
-   echo "1. $l " 
-   name=`echo $l | /usr/bin/cut -d "-" -f 2- | /usr/bin/cut -d "." -f 1 `
-   type=`echo $l | /usr/bin/cut -d "-" -f 1`
-   ext=`echo $l | /usr/bin/cut -d "." -f 2`
-                newname="${name}_${type}.${ext}" 
-   echo "2.  $name $type $ext : $newname " 
-
-#   /bin/mv $l $newname
-   if [ $? != 0 ] ; then
-      exit 0
-      echo "out..."
-   fi
-
-done
+    name="${l##*/}" 
+    ext=`echo $name | $(which cut) -d "."  -f 2 ` 
+    pth=${l%/*} 
+    echo "$l --- $pth --- $name $ext " 
 
 
-
-
-
-
-
-
-exit 0 
-###################################### compare dates 
-
-for l in `/usr/bin/cat a.txt`
-do
-   echo "1. $l " 
-   epoccrtime=`/usr/bin/stat -c %X $l` 
-   echo "Createtime : "`/usr/bin/stat -c %X $l `" :: "`/usr/bin/stat -c %x $l`
-   epocmdtime=`/usr/bin/stat -c %Y $l ` 
-   echo "Modifytime : "`/usr/bin/stat -c %Y $l `" :: "`/usr/bin/stat -c %y $l`
+   epoccrtime=`$(which stat) -c %X $l`
+#   echo "Createtime : "`$(which stat) -c %X $l `" :: "`/usr/bin/stat -c %x $l`
+   epocmdtime=`$(which stat) -c %Y $l `
+#   echo "Modifytime : "`$(which stat) -c %Y $l `" :: "`/usr/bin/stat -c %y $l`
    let diff=$epocmdtime-$epoccrtime
    if [ $epocmdtime -gt $epoccrtime  ] ; then
       echo "Create time is older "
-      echo "Modifytime : "`/usr/bin/stat -c %x $l`
-      DT=`/usr/bin/stat -c %x $l`
+#      echo "Modifytime : "`$(which stat) -c %x $l`
+      DT=`$(which stat) -c %x $l`
    else
-      echo "Modify time is older "
-      echo Modifytime" : "`/usr/bin/stat -c %y $l`
-      DT=`/usr/bin/stat -c %y $l`
+#      echo "Modify time is older "
+#      echo Modifytime" : "`$(which stat) -c %y $l`
+      DT=`$(which stat) -c %y $l`
    fi
-echo $DT |  cut -d "." -f 1  | tr '\n' ' ' | sed  -e 's/[^0-9]/ /g'  -e 's/^*//g' -e 's/ *$//g' | tr -s ' '  | sed  's/ //g'
-echo ""
+   newname=`echo $DT |  $(which cut) -d "." -f 1  | $(which tr) '\n' ' ' | $(which sed)  -e 's/[^0-9]/ /g'  -e 's/^*//g' -e 's/ *$//g' | tr -s ' '  | $(which sed)  's/ //g'` 
+   newname1=`$(which date) +%N`
+#   echo ***********"time considered is ${newname}_${newname1}"
+   newpath=${newfolder}/${newname}_${newname1}.${ext}
+   echo ".............${l} ${newpath}" 
+   if [  -e ${newpath}  ]  ;  then
+       echo "Skipping ${l} "
+   else
+	$(which mv)  $l ${newpath}
+   	if [ $? != 0 ] ; then
+      	  echo "Failed Moving ${l}  to ${newpath} ..."
+   	fi
+   fi 
 done
-
-
-exit 0
-############# For VID_<dt>_<time>.ext type of files  and  IMG_<dt>_<time>.ext 
-for l in `/usr/bin/cat a.txt`
-do
-   echo "1. $l " 
-   name=`echo $l | /usr/bin/cut -d "_" -f 2- | /usr/bin/cut -d "." -f 1 `
-   type=`echo $l | /usr/bin/cut -d "_" -f 1`
-   ext=`echo $l | /usr/bin/cut -d "." -f 2`
-                newname="${name}_${type}.${ext}" 
-   echo "2.  $name $type $ext : $newname " 
-
-   /usr/bin/mv $l $newname
-   if [ $? != 0 ] ; then
-      exit 0
-      echo "out..."
-   fi
-
-done
-
